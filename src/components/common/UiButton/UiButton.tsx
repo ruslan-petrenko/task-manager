5c15a4628908e073c8cd317d1e22253cacbe2759
Ruslan Petrenko <106350864+ruslan-petrenko@users.noreply.github.com>
2026-06-09 12:45:18 +0300

    Bugfix/tm 4 (#4)
    * Remove deprecated dark mode variant syntax from CSS files across components to ensure consistency and compatibility with updated styling practices.
    
    * Refactor NavBar component to use UiButton for theme toggle functionality and update UiButton to allow optional label prop, enhancing flexibility in button usage.

--- src/components/common/UiButton/UiButton.tsx
+++ src/components/common/UiButton/UiButton.tsx
@@ -2,16 +2,17 @@ interface ButtonProps {
   children?: React.ReactNode;
   onClick: () => void;
   className: string;
-  label: string;
+  label?: string;
 }
+
 export default function UiButton(props: ButtonProps) {
-  const { children, onClick, className, label } = props;
+  const { children, onClick, className, label = '' } = props;
   return (
     <button
       className={`${className} cursor-pointer`}
       onClick={onClick}
     >
-      {label ?? children}
+      {children ?? label}
     </button>
   );
 }
