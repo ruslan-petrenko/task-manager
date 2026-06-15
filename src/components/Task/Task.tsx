947a5273a18104b05d5798541adf1593d27f2a0e
ruslan-petrenko <ruslanpetrenko2203@gmail.com>
2026-06-15 10:19:37 +0300

    Refactor task management by removing JSON server and implementing Express API
    - Delete db.json file and remove json-server dependency from package.json.
    - Introduce a new Express server with task management routes for CRUD operations.
    - Create shared types for task management and update component imports accordingly.
    - Refactor KanbanBoard, KanbanColumn, and Task components to utilize the new shared types.
    - Implement task routes in the server for handling task data.

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,4 +1,4 @@
-import { type Task } from '@/types';
+import { type Task } from '../../../shared/types';
 import UiButton from '@/components/common/UiButton/UiButton';
 import useTask from '@/hooks/useTask';
 import { memo } from 'react';
